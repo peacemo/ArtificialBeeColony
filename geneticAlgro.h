3cd9a0970473cf0e539b9c329b9abf8bec9a5e97
peacemo <1321500713@qq.com>
2021-10-08 10:40:33 +0800

    complete cross logic

--- geneticAlgro.h
+++ geneticAlgro.h
@@ -41,6 +41,51 @@ int RouletteWheelSelection() {
     return Selection;
 }
 
+Food* cross(int f1, int f2, Food *foods) {
+    Food tempFood;
+    Food childrenFoods[2];
+
+    fdcpy(foods[f1], childrenFoods[0]); // 将父代的两个食物源先赋值给临时变量，杂交的时候对临时变量进行操作
+    fdcpy(foods[f2], childrenFoods[1]);
+
+    fdcpy(foods[f1], tempFood);  // 临时保存 f1，用于变量交换的中间变量
+
+    int randIndex = rand() % ((GoodsNum - GoodsNum / 3) - 0 + 1) + 0; // 随机选取交叉序列的起始位置[0, (GoodsNum - GoodsNum / 3)]
+
+    // 首先将 f2 的相应序列给到 f1
+    for (int iter = randIndex; iter < randIndex + (GoodsNum / 3); ++iter) { // 取出从随机位置开始，向后至“总数的三分之一”个元素
+        /**
+         *  全部迭代完后的效果就是
+         *  从 food2 中选取一段序列
+         *  从 food1 中删除掉这些序列中的值
+         *  再将这些值拼接到 food1 的末尾
+         * */
+        int currentElement = childrenFoods[1].getSequence(iter);
+        childrenFoods[0].removeFromSequence(currentElement); // (CODE_LENTH^2) 从自身的序列中删除交叉对象的该元素
+//        tempFood.addToEndOfSequence(currentElement); // 再将其置于序列的末尾
+        childrenFoods[0].addIntoSequence(iter, currentElement); // 再将其置于序列的相应位置
+    }
+    enSimpleCode(childrenFoods[0]);
+
+    // 再将 temp( f1的值 )的相应序列给到 f2
+    for (int iter = randIndex; iter < randIndex + (GoodsNum / 3); ++iter) { // 取出从随机位置开始，向后至“总数的三分之一”个元素
+        /**
+         *  全部迭代完后的效果就是
+         *  从 food2 中选取一段序列
+         *  从 food1 中删除掉这些序列中的值
+         *  再将这些值拼接到 food1 的末尾
+         * */
+        int currentElement = tempFood.getSequence(iter);
+        childrenFoods[1].removeFromSequence(currentElement); // (CODE_LENTH^2) 从自身的序列中删除交叉对象的该元素
+//        tempFood.addToEndOfSequence(currentElement); // 再将其置于序列的末尾
+        childrenFoods[1].addIntoSequence(iter, currentElement); // 再将其置于序列的相应位置
+    }
+    enSimpleCode(childrenFoods[1]);
+
+    return childrenFoods;
+
+}
+
 /*!
  * 通过父代种群产生新的子代种群
  * @param foods
@@ -49,7 +94,7 @@ void generateNewFoods(Food *foods) {
     Food newFoods[FoodsNum];
     int newFoodsLength = 0;
 
-    while(newFoodsLength < FoodsNum) {
+    while(newFoodsLength < FoodsNum) {  // 在子代规模达到种群大小之前，不断地产生新的子代，并添加进入子代集
         // 选择两个不同的个体
         int selection1 = RouletteWheelSelection();
         int selection2 = selection1;
@@ -57,11 +102,16 @@ void generateNewFoods(Food *foods) {
             selection2 = RouletteWheelSelection();
         }
         cout << selection1 << ", " << selection2 << endl;
-
         // TODO: 两个个体的交叉逻辑
-        cross(selection1, selection2);
+        Food *children = cross(selection1, selection2, foods);
+        newFoods[newFoodsLength] = children[0];
+        newFoodsLength++;
+        newFoods[newFoodsLength] = children[1];
+        newFoodsLength++;
+
     }
 
+    // TODO: 变异逻辑
 
 
 
